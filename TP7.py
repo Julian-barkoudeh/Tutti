e9d5154b18b26b117ca8d55404337d863bd172c3
Julian-barkoudeh <71441641+Julian-barkoudeh@users.noreply.github.com>
2023-02-23 23:00:37 +0100

    Update TP7.py

--- TP7.py
+++ TP7.py
@@ -1,17 +1,17 @@
 def simplification0(exp):
     somme = 0
     expNew = ""
-    action = "+"
-    cpt = 0
+    action = "+" # variable pour savoir le sign de la variable 
+    cpt = 0 #Variable pour savoir la position de l'inconnu dans la nouvelle expression 
     for caractere in exp:
-        if (caractere.isdigit()):
+        if (caractere.isdigit()): #Il s'agit d'un nombre entier 
             if(action == "+") :
                 somme += int(caractere)
             else :
                 somme -= int(caractere)
-        elif (caractere == "+" or caractere == "-") :
+        elif (caractere == "+" or caractere == "-") : #Il s'agit des symboles + ou -
             action = caractere
-        else :
+        else : #Il s'agit des inconnus x et y 
             if cpt == 0 :
                 expNew += caractere
             else :
